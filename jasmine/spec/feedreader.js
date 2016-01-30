7862a9f4e43ae32dab03790ebf4f27e97c6a8b83
Brody Chen <rufengch@gmail.com>
2016-01-30 13:21:57 -0800

    edit New Feed Selection

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -79,9 +79,10 @@ $(function() {
         var content, newContent;
 
         beforeEach(function(done) {
-            loadFeed(0, function() { // load 1st time
+            // load 1st time, use parameter 1 now and 0 in 2nd load so that the page is in original state after test
+            loadFeed(1, function() {
                 content = $('.feed').text(); // get 1st content when 1st load completes
-                loadFeed(1, function() { // load 2nd time
+                loadFeed(0, function() { // load 2nd time
                     newContent = $('.feed').text(); // get 2nd content when 2nd load completes
                     done(); // the asynchronous function is done
                 });
@@ -89,7 +90,6 @@ $(function() {
         });
 
         it('Content changes when new feed loaded', function(done) {
-            loadFeed(0); // display the page in original state
             expect(newContent).not.toBe(content);
             done();
         });
